4a6dbfad3a8fc312def9dd87cf13dad1c0bc998e
GWojciech <woitek@autograf.pl>
2018-04-21 12:26:55 +0200

    Przed reinstalacją środowiska, awaryjna kopia

--- app/src/main/java/com/golabek/wkck/serviceclassa/tabbed/group1/TableGroup1Activity.java
+++ app/src/main/java/com/golabek/wkck/serviceclassa/tabbed/group1/TableGroup1Activity.java
@@ -2,10 +2,14 @@ package com.golabek.wkck.serviceclassa.tabbed.group1;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.golabek.wkck.serviceclassa.R;
 
@@ -13,8 +17,24 @@ public class TableGroup1Activity extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.table_group3, container, false);
+        View view = inflater.inflate(R.layout.table_group1, container, false);
+        TableLayout tableLayout = (TableLayout) view.findViewById(R.id.tableLayout1);
+        TableRow tableRow = (TableRow) view.findViewById(R.id.tableTableRow3);
+        TableRow newTableRow = new TableRow(this.getContext());
+        TextView textView = new TextView(tableRow.getContext());
+        textView.setText("1");
+        newTableRow.addView(textView);
+        textView = new TextView(tableRow.getContext());
+        textView.setText("Korona");
+        newTableRow.addView(textView);
+//        textView.setText("43");
+//        newTableRow.addView(textView);
+        tableLayout.addView(newTableRow);
 
         return  view;
+    }
+
+    public void setTeamsInTheView(){
+
     }
 }
